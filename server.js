9a6b29e1c6eb69137706f153ad950241833e46e2
Nikhil Bansal <nbansal@uncc.edu>
2023-09-15 02:44:45 -0400

    Added server, Axios, and ChartJS

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 // Budget API
 
 const express = require('express');
-const cors = require('cors');
+// const cors = require('cors');
 const app = express();
 const port = 3000;
 
-app.use(cors());
+// app.use(cors());
+app.use('/', express.static('public'));
 
 const budget = {
     myBudget: [
